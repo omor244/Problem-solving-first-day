2342835ef16e45b6a626cad724848078b590e21e
Abdur55 <mrrahim972@gmail.com>
2026-01-08 07:55:42 +0600

    first commit

---
+++ Problems.js
@@ -0,0 +1,26 @@
+
+
+const Problem1 = (input) => {
+
+    const str = "hello"
+
+    let reverse = ""
+
+    for (let i = str.length - 1; i >= 0; i--) {
+
+    reverse += str[i]
+}
+
+return reverse
+    
+}
+
+
+
+
+console.log(Problem1("Hello"))
+
+
+
+
+
